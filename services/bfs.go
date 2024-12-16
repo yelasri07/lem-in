26890eb8d451505570206030b485d7532390bf27
yelasri <elasriyoussef604@gmail.com>
2024-12-16 15:22:15 +0100

    Paths struct

--- services/bfs.go
+++ services/bfs.go
@@ -24,7 +24,18 @@ func (g *GraphData) BFSHelper() {
 
 		lastRoomCurrent := currentPath[len(currentPath)-1]
 		if lastRoomCurrent == g.End {
-			g.Paths = append(g.Paths, currentPath)
+			intersectionPoint := true
+			for i := 1; i < len(currentPath)-1; i++ {
+				for j := 0; j < len(g.Paths); j++ {
+					if slices.Contains(g.Paths[j].rooms, currentPath[i]) {
+						intersectionPoint = false
+					}
+				}
+			}
+			if intersectionPoint {
+				path := Paths{len: len(currentPath), rooms: currentPath}
+				g.Paths = append(g.Paths, &path)
+			}
 			continue
 		}
 
@@ -39,11 +50,10 @@ func (g *GraphData) BFSHelper() {
 	}
 
 	g.FindBestPaths()
-
 }
 
 func (g *GraphData) FindBestPaths() {
 	for _, path := range g.Paths {
-		fmt.Println(strings.Join(path, " -> "))
+		fmt.Println(strings.Join(path.rooms," -> ") , "|| len =", path.len)
 	}
 }
