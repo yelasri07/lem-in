6599240ce3aff68c97955458c3c6a93d3a535f3b
Africano07 <elasriyoussef604@gmail.com>
2024-12-20 17:02:31 +0100

    Print ants steps

--- services/printSteps.go
+++ services/printSteps.go
@@ -2,15 +2,16 @@ package services
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func PrintSteps(paths []*PathInfos, nbAnts int) {
-	Ants := make([][]int, len(paths)) // had ants hiya nmel li ghadi ydouz me3a dak lpath wach fhemti wela mafhmtich
+	Ants := make([]*PathInfos, nbAnts) // had ants hiya nmel li ghadi ydouz me3a dak lpath wach fhemti wela mafhmtich
 
 	for _, p := range paths {
 		fmt.Println(p)
 	}
+
+	fmt.Println("-------------------")
 
 	for i := 1; i <= nbAnts; i++ {
 		petitPath := paths[0]
@@ -23,20 +24,22 @@ func PrintSteps(paths []*PathInfos, nbAnts int) {
 		}
 
 		petitPath.len++
-		Ants[k] = append(Ants[k], i)
+		Ants[i-1] =  paths[k]
 	}
 
-	printAnts := make(map[string]*PathInfos)
+	// printAnts := make([]*PathInfos, 0)
 
-	for i := 0; i < len(Ants); i++ {
-		for j := 0; j < len(Ants[i]); j++ {
-			printAnts["L"+strconv.Itoa(Ants[i][j])] = paths[i]
-		}
+	// for i := 0; i < len(Ants); i++ {
+	// 	for j := 0; j < len(Ants[i]); j++ {
+	// 		printAnts["L"+strconv.Itoa(Ants[i][j])] = paths[i]
+	// 	}
+	// }
+
+	for i, c := range Ants{
+		fmt.Printf("nemla %v flpath : %v\n", i+1, c.Path)
 	}
 
-	fmt.Println(Ants)
-
-	for a, p := range printAnts {
-		fmt.Printf("nemla %v lpath li ghadi tmchi fih : %v\n", a , p.Path)
-	}
+	// for a, p := range printAnts {
+	// 	fmt.Printf("nemla %v lpath li ghadi tmchi fih : %v\n", a , p.Path)
+	// }
 }
