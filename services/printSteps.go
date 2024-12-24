d29e4b3cb0ccb5a784f3e8e6f55f73e328ca33ce
Africano07 <elasriyoussef604@gmail.com>
2024-12-24 21:40:18 +0100

    print Steps

--- services/printSteps.go
+++ services/printSteps.go
@@ -26,18 +26,34 @@ func PrintSteps(paths []*PathInfos, nbAnts int, end string) {
 		ants[i-1] = append(ants[i-1], shortPath.Path...)
 	}
 
+	for i := 0; i < len(ants); i++ {
+		if len(ants[i]) == 1 {
+			ants[i] = append(ants[i], end)
+		}
+	}
+
+	fmt.Println(ants)
+
 	turns := [][]string{}
 	turn := []string{}
 	step := []string{}
 	for !allPathsEmpty(ants) {
+
+		badTunnel := false
 
 		for i := 0; i < len(ants); i++ {
 			if len(ants[i]) == 0 {
 				continue
 			}
 
-			if !slices.Contains(turn, ants[i][0]) {
-				s := fmt.Sprintf("L%v-%v", i+1, ants[i][0])
+			s := fmt.Sprintf("L%v-%v", i+1, ants[i][0])
+			if len(ants[i]) == 2 && ants[i][0] == end && ants[i][1] == end {
+				if !badTunnel {
+					ants[i] = ants[i][2:]
+					step = append(step, s)
+					badTunnel = true
+				}
+			} else if !slices.Contains(turn, ants[i][0]) {
 				if ants[i][0] != end {
 					turn = append(turn, ants[i][0])
 				}
@@ -55,7 +71,7 @@ func PrintSteps(paths []*PathInfos, nbAnts int, end string) {
 	}
 
 	for _, turn := range turns {
-		fmt.Println(strings.Join(turn," "))
+		fmt.Println(strings.Join(turn, " "))
 	}
 }
 
