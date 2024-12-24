54842335cc679c47fc8e612c67f8f4dd9089afb9
Africano07 <elasriyoussef604@gmail.com>
2024-12-24 21:15:37 +0100

    Print Steps using slice

--- services/PrintTurns.go
+++ services/PrintTurns.go
@@ -1,87 +1,70 @@
 package services
 
-import (
-	"fmt"
-	"slices"
-	"strings"
-)
+// func PrintTurns(g []AntsGroup, er string) {
+// 	Turns := [][]string{}
+// 	turn := []string{}
+// 	ar := []string{}
+// 	awor := GetAntswithOneRoom(g)
+// 	for !allPathsEmpty(g) {
+// 		for j := 0; j < len(g); j++ {
+// 			for i := 0; i < len(g[j].Ants); i++ {
+// 				if len(g[j].Paths[i]) == 0 {
+// 					continue
+// 				}
+// 				if slices.Contains(awor, g[j].Ants[i]) && NotinTurn(ar, awor) {
+// 					continue
+// 				}
+// 				if !contains(turn, g[j].Paths[i][0]) {
+// 					if g[j].Paths[i][0] != er {
+// 						turn = append(turn, g[j].Paths[i][0])
+// 					}
+// 					s := fmt.Sprintf("L%d-%s", g[j].Ants[i], g[j].Paths[i][0])
+// 					ar = append(ar, s)
+// 					g[j].Paths[i] = g[j].Paths[i][1:]
+// 				}
+// 			}
+// 		}
+// 		if len(ar) > 0 {
+// 			Turns = append(Turns, ar)
+// 		}
+// 		turn = []string{}
+// 		ar = []string{}
+// 	}
 
-func allPathsEmpty(groups []AntsGroup) bool {
-	for _, group := range groups {
-		for _, path := range group.Paths {
-			if len(path) > 0 {
-				return false
-			}
-		}
-	}
-	return true
-}
+// 	for _, ele := range Turns {
+// 		fmt.Println(strings.Join(ele, " "))
+// 	}
+// }
 
-func PrintTurns(g []AntsGroup, er string) {
-	Turns := [][]string{}
-	turn := []string{}
-	ar := []string{}
-	awor := GetAntswithOneRoom(g)
-	for !allPathsEmpty(g) {
-		for j := 0; j < len(g); j++ {
-			for i := 0; i < len(g[j].Ants); i++ {
-				if len(g[j].Paths[i]) == 0 {
-					continue
-				}
-				if slices.Contains(awor, g[j].Ants[i]) && NotinTurn(ar, awor) {
-					continue
-				}
-				if !contains(turn, g[j].Paths[i][0]) {
-					if g[j].Paths[i][0] != er {
-						turn = append(turn, g[j].Paths[i][0])
-					}
-					s := fmt.Sprintf("L%d-%s", g[j].Ants[i], g[j].Paths[i][0])
-					ar = append(ar, s)
-					g[j].Paths[i] = g[j].Paths[i][1:]
-				}
-			}
-		}
-		if len(ar) > 0 {
-			Turns = append(Turns, ar)
-		}
-		turn = []string{}
-		ar = []string{}
-	}
+// func contains(slice []string, item string) bool {
+// 	for _, v := range slice {
+// 		if v == item {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
-	for _, ele := range Turns {
-		fmt.Println(strings.Join(ele, " "))
-	}
-}
+// func GetAntswithOneRoom(g []AntsGroup) []int {
+// 	Ants := []int{}
+// 	for _, ele := range g {
+// 		for i, a := range ele.Paths {
+// 			if len(a) == 1 {
+// 				Ants = append(Ants, ele.Ants[i])
+// 			}
+// 		}
+// 	}
+// 	return Ants
+// }
 
-func contains(slice []string, item string) bool {
-	for _, v := range slice {
-		if v == item {
-			return true
-		}
-	}
-	return false
-}
-
-func GetAntswithOneRoom(g []AntsGroup) []int {
-	Ants := []int{}
-	for _, ele := range g {
-		for i, a := range ele.Paths {
-			if len(a) == 1 {
-				Ants = append(Ants, ele.Ants[i])
-			}
-		}
-	}
-	return Ants
-}
-
-func NotinTurn(ar []string, awor []int) bool {
-	for i := 0; i < len(ar); i++ {
-		for j := 0; j < len(awor); j++ {
-			ants := fmt.Sprintf("L%d-", awor[j])
-			if strings.HasPrefix(ar[i], ants) {
-				return true
-			}
-		}
-	}
-	return false
-}
+// func NotinTurn(ar []string, awor []int) bool {
+// 	for i := 0; i < len(ar); i++ {
+// 		for j := 0; j < len(awor); j++ {
+// 			ants := fmt.Sprintf("L%d-", awor[j])
+// 			if strings.HasPrefix(ar[i], ants) {
+// 				return true
+// 			}
+// 		}
+// 	}
+// 	return false
+// }
--- services/printSteps.go
+++ services/printSteps.go
@@ -1,10 +1,18 @@
 package services
 
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
 func PrintSteps(paths []*PathInfos, nbAnts int, end string) {
-	var Grps []AntsGroup
-	ants := []int{}
-	currentPath := [][]string{}
-	grpAntCount := 0
+
+	ants := make([][]string, nbAnts)
+
+	for _, p := range paths {
+		fmt.Println(p)
+	}
 
 	for i := 1; i <= nbAnts; i++ {
 		shortPath := paths[0]
@@ -15,25 +23,47 @@ func PrintSteps(paths []*PathInfos, nbAnts int, end string) {
 		}
 		shortPath.len++
 
-		if grpAntCount != len(paths) {
-			ants = append(ants, i)
-			currentPath = append(currentPath, shortPath.Path)
-			grpAntCount++
-		}
-
-		if grpAntCount == len(paths) {
-			st := &AntsGroup{Ants: ants, Paths: currentPath}
-			Grps = append(Grps, *st)
-			ants = []int{}
-			currentPath = [][]string{}
-			grpAntCount = 0
-		}
-
-	}
-	if len(ants) != 0 && len(currentPath) != 0 {
-		st := &AntsGroup{Ants: ants, Paths: currentPath}
-		Grps = append(Grps, *st)
+		ants[i-1] = append(ants[i-1], shortPath.Path...)
 	}
 
-	PrintTurns(Grps, end)
+	turns := [][]string{}
+	turn := []string{}
+	step := []string{}
+	for !allPathsEmpty(ants) {
+
+		for i := 0; i < len(ants); i++ {
+			if len(ants[i]) == 0 {
+				continue
+			}
+
+			if !slices.Contains(turn, ants[i][0]) {
+				s := fmt.Sprintf("L%v-%v", i+1, ants[i][0])
+				if ants[i][0] != end {
+					turn = append(turn, ants[i][0])
+				}
+				ants[i] = ants[i][1:]
+				step = append(step, s)
+			}
+
+		}
+
+		turns = append(turns, step)
+
+		turn = []string{}
+		step = []string{}
+
+	}
+
+	for _, turn := range turns {
+		fmt.Println(strings.Join(turn," "))
+	}
+}
+
+func allPathsEmpty(groups [][]string) bool {
+	for _, group := range groups {
+		if len(group) > 0 {
+			return false
+		}
+	}
+	return true
 }
